11b19f0ba335a08435b2419963e386da42139f5a
aaltukhov <altukhov.artem.dev@gmail.com>
2021-06-27 17:47:15 +0300

    adds the TaskController, adds a DI to inject MockTaskRepo

---
+++ TaskProject/Controllers/TaskController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskProject.Models;
+using Task = TaskProject.Models.Task;
+
+namespace TaskProject.Controllers
+{
+	public class TaskController : Controller
+	{
+		private readonly ITaskRepository _taskRepository;
+
+		public TaskController(ITaskRepository taskRepository)
+		{
+			this._taskRepository = taskRepository;
+		}
+		public IActionResult List()
+		{
+			IEnumerable<Task> tasks = _taskRepository.GetAll();
+			return View(tasks);
+		}
+	}
+}
--- TaskProject/Startup.cs
+++ TaskProject/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskProject.Models;
 
 namespace TaskProject
 {
@@ -16,6 +17,7 @@ namespace TaskProject
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddScoped<ITaskRepository, MockTaskRepository>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
